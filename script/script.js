9227cb27f0c88e69a86073b6757a22c2c42b4f10
ceresaxd <alvarezalvarezc05@gmail.com>
2026-06-03 14:17:01 -0600

    si

--- script/script.js
+++ script/script.js
@@ -15,4 +15,28 @@ function cambiarSlide() {
     slides[indice].classList.add("activo");
 }
 
-setInterval(cambiarSlide, 4000);
+setInterval(cambiarSlide, 5000);
+
+
+//animacion
+const secciones = document.querySelectorAll(".seccion");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+        } else {
+            entry.target.classList.remove("visible");
+        }
+
+    });
+
+}, {
+    threshold: 0.2
+});
+
+secciones.forEach(seccion => {
+    observer.observe(seccion);
+});
