d19077adb73a937cf77bc7829d4e6d5a6b0cfc54
ceresaxd <alvarezalvarezc05@gmail.com>
2026-06-05 16:58:37 -0600

    1

--- script/script.js
+++ script/script.js
@@ -1,25 +1,8 @@
-const slides = document.querySelectorAll(".slide");
 
-let indice = 0;
-
-function cambiarSlide() {
-
-    slides[indice].classList.remove("activo");
-
-    indice++;
-
-    if (indice >= slides.length) {
-        indice = 0;
-    }
-
-    slides[indice].classList.add("activo");
-}
-
-setInterval(cambiarSlide, 5000);
 
 
 //boton Nav
-const a = 0;
+
 const btnNav = document.getElementById("btnNav");
 const menu = document.getElementById("navMenu");
 
