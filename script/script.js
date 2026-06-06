c934734c7ae2c313939941ffc0ee3d8f023db41c
ceresaxd <alvarezalvarezc05@gmail.com>
2026-06-05 18:07:36 -0600

    2

--- script/script.js
+++ script/script.js
@@ -1,6 +1,27 @@
+//color header
+const header = document.querySelector("header");
+const hero = document.querySelector(".hero");
 
+window.addEventListener("scroll", () => {
+    const heroTop = hero.offsetTop;
 
+    if (window.scrollY >= heroTop) {
+        header.classList.add("hero-color");
+    } else {
+        header.classList.remove("hero-color");
+    }
+});
+//logo
+const logos = document.querySelectorAll(".logo");
+const home = document.querySelector(".home");
 
+logos.forEach(logo => {
+    logo.addEventListener("click", () => {
+        home.scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
 //boton Nav
 
 const btnNav = document.getElementById("btnNav");
